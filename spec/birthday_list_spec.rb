3419feaa14ce03ba2cadc139d057d84ff6d1b2ba
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-11 12:27:46 +0000

    Refactor and add before hooks.

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -4,42 +4,29 @@ require 'birthday_list.rb'
 
 describe Birthday do
   it 'has the name of a person' do
-    birthday = Birthday.new('Hannah', '14-Feb')
-    expect(birthday.person).to eq('Hannah')
+    birthday = Birthday.new('Letty', '11-2-1991')
+    expect(birthday.person).to eq('Letty')
   end
   it 'has the date of a person' do
-    birthday = Birthday.new('Hannah', '14-Feb')
-    expect(birthday.date).to eq('14-Feb')
-  end
-  describe '#age' do
-    it "returns the person's age" do
-      birthday = Birthday.new('Hannah', '11-02-1986')
-      expect(birthday.age).to eq(35)
-    end
+    birthday = Birthday.new('Letty', '11-2-1991')
+    expect(birthday.date).to eq('11-2-1991')
   end
 
 describe BirthdayList do
-  it 'has somewhere to store birthdays' do
-    expect(subject.birthday_list).to eq([])
+  before (:each) do
+    @letty_bday = Birthday.new('Letty', '11-2-1991')
+    @birthday = @letty_bday.entry
+    subject.new_entry(@birthday)
   end
   it 'adds the new entry to the list' do
-    birthday = Birthday.new('Hannah', '14-Feb').entry
-    subject.new_entry(birthday)
-    expect(subject.birthday_list).to eq([birthday])
+    expect(subject.birthday_list).to eq([@birthday])
   end
   it 'prints the birthday list' do
-    birthday = Birthday.new('Hannah', '14-Feb').entry
-    subject.new_entry(birthday)
-    expect { subject.show_list }.to output("Hannah, 14-Feb\n").to_stdout
+    expect { subject.show_list }.to output("Letty, 11-2-1991\n").to_stdout
   end
-  describe '#today' do
     it "checks whose birthday it is today" do
-      letty_bday = Birthday.new('Letty', '11-2-1991')
-      birthday = letty_bday.entry
-      subject.new_entry(birthday)
-      age = letty_bday.age
+      age = @letty_bday.age
       expect { subject.today(age) }.to output("It's Letty's birthday today, they are 30\n").to_stdout
     end
   end
-end
 end
